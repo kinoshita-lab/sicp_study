7a27e3fd7264520f2dc0be6b866af70e27584280
kinoshita-lab <saita@kinoshita-lab.org>
2015-02-09 22:08:41 +0900

    2.68

--- chapter2-4.scm
+++ chapter2-4.scm
@@ -125,23 +125,225 @@
 (define (union-set set1 set2)
  (cond ((null? set1) set2)
        ((null? set2) set1)
-       (else
-         (if (null? set1) 
-             set2
-             (let ((x1 (car set1)) 
-                   (x2 (car set2)))
-               (cond ((= x1 x2)
-                      (union-set (cdr set1) set2))
-                     ((< x1 x2)
-                      (cons x1 (union-set (cdr set1)
-                                          set2)))
-                     ((> x1 x2)
-                      (cons x2 (union-set set1 (cdr set2))))))))))
+       (else (let ((x1 (car set1)) 
+             (x2 (car set2)))
+         (cond ((= x1 x2)
+                (union-set (cdr set1) set2))
+               ((< x1 x2)
+                (cons x1 (union-set (cdr set1)
+                                    set2)))
+               ((> x1 x2)
+                (cons x2 (union-set set1 (cdr set2)))))))))
 (define list1 '(2 4 6 8))
 (define list2 '(1 3 5 7))
 (union-set list1 list2) 
-(trace union-set)
+;gosh>(1 2 3 4 5 6 7 8)
 
 
 
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
 
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+
+; 2.63
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define tree216-1
+  (make-tree '7 (make-tree '3 '1 '5) (make-tree '9 '() '11)))
+
+(define tree216-2
+  (make-tree '3 '1 (make-tree '7 '5 (make-tree '9 '() '11))))
+
+(define tree216-3
+  (make-tree '5 (make-tree '3 '1 '()) (make-tree '9 '7 '11)))
+
+(trace tree->list-1)
+(trace tree->list-2)
+
+(tree->list-1 tree216-1)
+(tree->list-2 tree216-1)
+
+(tree->list-1 tree216-2)
+(tree->list-2 tree216-2)
+
+(tree->list-1 tree216-3)
+(tree->list-2 tree216-3)
+
+; 問題がまともに動かないし　なんじゃこれ
+; 2番目のほうが１章でやった iter方式だからこっちのがよさげ。
+
+
+; 2.64
+
+; a
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(trace list->tree)
+(trace partial-tree)
+(trace make-tree)
+(list->tree '(1 3 5 7 9 11))
+
+; こんなのでてきた
+; (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+; 絵を描くのがめんどい
+  ;;     5
+  ;;  /     \
+  ;; 1       9
+  ;;   \    / \ 
+  ;;    3  7  11
+; quatientってなんだっけ 商
+; traceはってみる
+;; gosh> CALL list->tree (1 3 5 7 9 11)
+;;   CALL partial-tree (1 3 5 7 9 11) 6
+;;     CALL partial-tree (1 3 5 7 9 11) 2
+;;       CALL partial-tree (1 3 5 7 9 11) 0
+;;       RETN partial-tree (() 1 3 5 7 9 11)
+;;       CALL partial-tree (3 5 7 9 11) 1
+;;         CALL partial-tree (3 5 7 9 11) 0
+;;         RETN partial-tree (() 3 5 7 9 11)
+;;         CALL partial-tree (5 7 9 11) 0
+;;         RETN partial-tree (() 5 7 9 11)
+;;         CALL make-tree 3 () ()
+;;         RETN make-tree (3 () ())
+;;       RETN partial-tree ((3 ...) 5 7 9 11)
+;;       CALL make-tree 1 () (3 () ())
+;;       RETN make-tree (1 () (3 () ()))
+;;     RETN partial-tree ((1 ...) 5 7 9 11)
+;;     CALL partial-tree (7 9 11) 3
+;;       CALL partial-tree (7 9 11) 1
+;;         CALL partial-tree (7 9 11) 0
+;;         RETN partial-tree (() 7 9 11)
+;;         CALL partial-tree (9 11) 0
+;;         RETN partial-tree (() 9 11)
+;;         CALL make-tree 7 () ()
+;;         RETN make-tree (7 () ())
+;;       RETN partial-tree ((7 () ()) 9 11)
+;;       CALL partial-tree (11) 1
+;;         CALL partial-tree (11) 0
+;;         RETN partial-tree (() 11)
+;;         CALL partial-tree () 0
+;;         RETN partial-tree (())
+;;         CALL make-tree 11 () ()
+;;         RETN make-tree (11 () ())
+;;       RETN partial-tree ((11 () ()))
+;;       CALL make-tree 9 (7 () ()) (11 () ())
+;;       RETN make-tree (9 (7 () ()) (11 () ()))
+;;     RETN partial-tree ((9 (7 () ()) (11 () ())))
+;;     CALL make-tree 5 (1 () (3 () ())) (9 (7 () ()) (11 () ()))
+;;     RETN make-tree (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+;;   RETN partial-tree ((5 (1 () (3 () ())) (9 (7 () ...) (11 () ()))))
+;; RETN list->tree (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+;; (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+;;
+;; 3つ拾ってきて木にするしてそれを合体する動きをしている
+
+; b
+; 要素が増えると　ばらして左右って気を作るステップが最大で3増えて、場合によっては新しい木にするために上の構造までその影響が及ぶので logNくらいじゃないかな。
+
+
+; 2.65
+;問題2.63と2.64の結果を使い, (釣合った)二進木として実装されている集合のunion-setとintersection-setを&Theta(n)で実装せよ.
+; こういうことなの？？ちがくない？
+; a
+(define (union-set-tree set1 set2)
+  (let ((tree1 (list->tree set1))
+        (tree2 (list->tree set2)))
+    (tree->list-2 (union-set tree1 tree2))))
+
+(define (intersection-se-tree set1 set2)
+  (let ((tree1 (list->tree set1))
+        (tree2 (list->tree set2)))
+    (tree->list-2 (intersection-set tree1 tree2))))
+
+; b
+; 増加の程度は木ってるからΘ(n)で行けると思う
+
+
+;; (define (lookup given-key set-of-records)
+;;   (cond ((null? set-of-records) false)
+;;         ((equal? given-key (key (car set-of-records)))
+;;          (car set-of-records))
+;;         (else (lookup given-key (cdr set-of-records)))))
+;; ↑のkeyってのがそもそも定義されてないし
+;; (key (car-set-of-elements))で取り出せるものと仮定するしかない
+;; (car set-of-records)はkeyとそこにバインドされている情報が返ってくる。
+;; このへん説明がなくてうざいなあ。
+
+
+; 2.66
+; element-of-set? 木っぽい編はこうだった        
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((= x (entry set)) true)
+;;         ((< x (entry set))
+;;          (element-of-set? x (left-branch set)))
+;;         ((> x (entry set))
+;;          (element-of-set? x (right-branch set)))))
+;;
+(define (lookup given-key set-of-records)
+  (let ((top-record (car set-of-records)))   
+    (cond ((null? set-of-records) false)
+          ((= x (key top-record)
+              (car set-of-records)))
+          ((< x (key top-record))
+              (lookup x (left-branch set-of-records)))
+          ((> x (key top-record))
+              (lookup x (right-branch set-of-records))))))
+;; 多分こんな感じ
+  
+        
---
+++ chapter2-5.scm
@@ -0,0 +1,148 @@
+;; 2.3.4 例: Huffman符号木
+(use slib)
+(require 'trace)
+; true/falseない
+(define false #f)
+(define true #t)
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+; 復号化手続き
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)    ; 記号
+                               (cadr pair))  ; 頻度
+                    (make-leaf-set (cdr pairs))))))
+
+;; 2.67
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+
+(decode sample-message sample-tree)
+; gosh> (A D A B B C A)
+
+;; 2.68
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+;; ↑で動くようにencode-symbolを考える問題
+
+
+; こんなんかいてみた
+(define (has-symbol a-symbol symbols)
+  (cond ((null? symbols) false)
+        ((eq? (car symbols) a-symbol) true)
+        (else (has-symbol a-symbol (cdr symbols)))))
+
+(has-symbol 'A (symbols sample-tree))
+(has-symbol 'Z (symbols sample-tree))
+;gosh> #t
+;gosh> #f
+;よさげ
+
+(define (encode-one bits letter subtree)
+  (if (leaf? subtree)
+      bits
+      (cond ((has-symbol letter (symbols (left-branch subtree)))
+             (encode-one (append bits '(0)) letter (left-branch subtree)))
+            ((has-symbol letter (symbols (right-branch subtree)))
+             (encode-one (append bits '(1)) letter (right-branch subtree)))
+            (else (error "bad bit -- ENCODE-ONE" bit)))))
+
+(encode-one '() 'A sample-tree)
+(encode-one '() 'B sample-tree)
+(encode-one '() 'C sample-tree)            
+(encode-one '() 'D sample-tree)
+; よさげ  
+;; gosh> (0)
+;; gosh> (1 0)
+;; gosh> (1 1 1)
+;; gosh> (1 1 0)
+
+;; 合体
+(define (encode-message message tree)
+  (define (has-symbol a-symbol symbols)
+    (cond ((null? symbols) false)
+          ((eq? (car symbols) a-symbol) true)
+          (else (has-symbol a-symbol (cdr symbols)))))
+
+  (define (encode-one bits letter subtree)
+    (if (leaf? subtree)
+        bits
+        (cond ((has-symbol letter (symbols (left-branch subtree)))  ; 左側にあったら
+               (encode-one (append bits '(0)) letter (left-branch subtree))) ; 0をつけて1段階掘り進む
+              ((has-symbol letter (symbols (right-branch subtree))) ; 右側にあったら
+               (encode-one (append bits '(1)) letter (right-branch subtree))) ;1をつけて1段階掘り進む
+              (else (error "bad bit -- ENCODE-ONE" bit)))))
+
+  (define (encode-iter bits message tree)
+    (if (null? message) bits
+        (append bits (encode-iter (encode-one '() (car message) tree) ; bits
+                     (cdr message) ; letter
+                     tree))))
+  (encode-iter '() message tree))
+
+(encode-message '(A D A B B C A) sample-tree)
+; gosh> (0 1 1 0 0 1 0 1 0 1 1 1 0)  
