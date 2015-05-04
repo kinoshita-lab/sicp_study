3d91ddb656c53db62768c2c2e1dbec5ea49e5ca4
kinoshita-lab <saita@kinoshita-lab.org>
2015-05-04 16:08:01 +0900

    3.14

--- chapter3-4.scm
+++ chapter3-4.scm
@@ -18,3 +18,27 @@ z
 ;; gosh> #0=(a b c . #0#)
 ;; こんなかんじになった。#0が循環してるっぽいかんじかな。
 
+;; 3.14
+;; loopの中を紙で動かしてみた。
+;; 一般にmysteryが何をするか説明せよ
+;; 'a 'b 'c 'd => 'd 'c 'b 'aってなる。
+;; 動かしてみる
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+(define v (list 'a 'b 'c 'd))
+v
+(define w (mystery v))
+w
+v
+;; gosh> mystery
+;; gosh> v
+;; gosh> (a b c d)
+;; gosh> w
+;; gosh> (d c b a)
+;; gosh> (a)
