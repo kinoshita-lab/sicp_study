aa0b894fb342452dbebc4aa6ceb299543fe4429e
kinoshita-lab <saita@kinoshita-lab.org>
2015-05-09 10:52:02 +0900

    3.3.4, 3.28

---
+++ chapter3-7.scm
@@ -0,0 +1,79 @@
+;;; 3.3.4 ディジタル回路のシミュレータ
+
+;; これうごかないよね
+;; 写経はする
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+(define d (make-wire))
+(define e (make-wire))
+(define s (make-wire))
+
+(or-gate a b d)
+(inverter c e)
+(and-gate d e s)
+
+;; まあここまで動くとして・・
+;; 半加算器
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+;; 全加算器
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+
+(define (inverter input output)
+  (define (inverter-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+;; and gate
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  'ok)
+;; これいるでしょ
+(define (logical-and a b)
+  (cond ((and (= 1 a) (= 1 b)) 1)
+        (else 0)))
+
+;; ===============================================
+;; 3.28 or gateを作る
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  'ok)
+;; これいるでしょ
+(define (logical-or a b)
+  (cond ((or (= 1 a) (= 1 b)) 1)
+        (else 0)))
