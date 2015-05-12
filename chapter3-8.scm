95a8a530574d13e17cde4537a0b4109991e0b21c
kinoshita-lab <saita@kinoshita-lab.org>
2015-05-12 12:54:04 +0900

    3.3.5 in progress

--- chapter3-8.scm
+++ chapter3-8.scm
@@ -68,11 +68,11 @@
                        me))
           ((and (has-value? product) (has-value? m1))
            (set-value! m2
-                       (/ (get-value product) (get-valule m1))
+                       (/ (get-value product) (get-value m1))
                        me))
           ((and (has-value? product) (has-value? m2))
            (set-value! m1
-                       (/ (get-value product) (get-valule m2))
+                       (/ (get-value product) (get-value m2))
                        me))))
   (define (process-forget-value)
     (forget-value! product me)
@@ -99,4 +99,85 @@
   me)
            
           
+(define (probe name connector)
+  (define (print-probe value)
+    (newline)
+    (display "Probe: ")
+    (display name)
+    (display " = ")
+    (display value))
+  (define (process-new-value)
+    (print-probe (get-value connector)))
+  (define (process-forget-value)
+    (print-probe "?"))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error "Unknown request -- PROBE" request))))
+  (connect connector me)
+  me)
 
+(define (make-connector)
+  (let ((value false) (informant false) (constraints '()))
+    (define (set-my-value newval setter)
+      (cond ((not (has-value? me))
+             (set! value newval)
+             (set! informant setter)
+             (for-each-except setter
+                              inform-about-value
+                              constraints))
+            ((not (= value newval))
+             (error "Contradiction" (list value newval)))
+            (else 'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor inform)
+          (begin (set! informant false)
+                 (for-each-except retrator
+                                  inform-about-no-value
+                                  constraints))
+          'ignored))
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+          (set! constraints
+                (cons new-constraint constraints)))
+      (if (has-value? me)
+          (inform-about-value new-constraint))
+      'done)
+    (define (me request)
+      (cond ((eq? request 'has-value?)
+             (if informant true false))
+            ((eq? request 'value) value)
+            ((eq? request 'set-value!) set-my-value)
+            ((eq? request 'forget) forget-my-value)
+            ((eq? request 'connect) connect)
+            (else (error "Unknown operation -- CONNECTOR"
+                         request))))
+    me))
+
+(define (for-each-except exception procedure list)
+  (define (looop items)
+    (cond ((null? items) 'done)
+          ((eq? (car items) exception) (loop (cdr items)))
+          (else (procedure (car items))
+                (loop (cdr items)))))
+  (loop list))
+
+(define (has-value? connector)
+  (connector 'has-value?))
+
+(define (get-value connector)
+  (connector 'value))
+
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+
+(define (forget-value!! connector retractor)
+  ((connect 'forget) retractor))
+
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+
+;; ↓ここまで全部定義すると上のやつが動くような気がするのでやってみるコーナー
