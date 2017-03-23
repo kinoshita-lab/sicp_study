06a20db7e48a9d8ef32a23dc06494e725af1f006
kinoshita-lab <saita@kinoshita-lab.org>
2017-03-23 12:39:30 +0900

    5.4

--- chapter5-1.scm
+++ chapter5-1.scm
@@ -59,3 +59,53 @@
  sqrt-done
  )
 
+;; 5.4: 次のそれぞれの手続きを実装するレジスタマシンを
+;; 規定せよ。それぞれのマシンについて、コントローラ命令列を書
+;; き、データパスを示す図を描け。
+;; a. 再帰的指数計算
+;; (define (expt b n)
+;;   (if (= n 0)
+;; 	  1
+;; 	  (* b (expt b (- n 1)))))
+;; factのをもとにした
+(controller
+ (assign b (op read))
+ (assign n (op read))
+ (assign continue (label expt-done)) 
+ expt-loop
+ (test (op =) (reg n) (const 0))
+ (branch (label base-case))
+ (save continue)
+ (save n)
+ (assign n (op -) (reg n) (const 1))
+ (assign continue (label after-expt))
+ (goto (label expt-loop))
+ after-expt
+ (restore n)
+ (restore continue)
+ (assign val (op *) (reg b) (reg val)) 
+ (goto (reg continue))
+ base-case
+ (assign val (const 1))
+ (goto (reg continue))
+ expt-done)
+;; b. 反復的指数計算
+;; (define (expt b n)
+;;   (define (expt-iter counter product)
+;; 	(if (= counter 0)
+;; 		product
+;; 		(expt-iter (- counter 1)
+;; 					(* b product))))
+;;   (expt-iter n 1))
+(controller
+ (assign b (op read))
+ (assign counter (op read))
+ (assign product (const 1))
+ expt-iter-loop
+ (test (op =) (reg counter) (const 0))
+ (goto label expt-done)
+ (assign product (op *) (reg b) (reg product))
+ (assign counter (op -) (reg counter) (const 1))
+ (goto (label expt-iter-loop))
+ expt-done
+ )
