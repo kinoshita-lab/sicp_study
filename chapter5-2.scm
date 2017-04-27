a22a931b9a367e74c912908a16043b743677784b
kinoshita-lab <saita@kinoshita-lab.org>
2017-04-27 12:41:05 +0900

    5.13

--- chapter5-2.scm
+++ chapter5-2.scm
@@ -361,3 +361,53 @@
 ;; save または restore されるレジスタの (重複なし) リスト。
 (ex512-2-fib-machine 'save-restore)
 ;;  (continue n val)
+
+
+;; 5.13
+;; ↓のerrorの部分でテーブルを更新すればよさげ
+;; (define (lookup-register name)
+;;        (let ((val (assoc name register-table)))
+;;          (if val
+;;              (cadr val)
+;;              (error "Unknown register:" name))))
+(load "./ch5-5.13.scm")
+;; fibで。
+(define fib-machine
+  (make-machine
+   (list (list '< <) (list '- -) (list '+ +))
+   '(
+	  (assign continue (label fib-done))
+	  fib-loop
+	  (test (op <) (reg n) (const 2))
+	  (branch (label immediate-answer))
+	  ;; Fib(n − 1) を求める準備
+	  (save continue)
+	  (assign continue (label afterfib-n-1))
+	  (save n) 										; n の古い値を保存
+	  (assign n (op -) (reg n) (const 1)) ; n を n-1 で上書き
+	  (goto (label fib-loop )) ; 再帰呼び出しの実行
+	  afterfib-n-1 										; リターン時に Fib(n − 1) は val に入っている
+	  (restore n)
+	  (restore continue )
+	  ;; Fib(n − 2) を求める準備
+	  (assign n (op -) (reg n) (const 2))
+	  (save continue )
+	  (assign continue (label afterfib-n-2 ))
+	  (save val) 									; Fib(n − 1) を保存
+	  (goto (label fib-loop))
+	  afterfib-n-2				; リターン時に Fib(n − 2) は val に入っている
+	  (assign n (reg val)) 		; n には Fib(n − 2) が入る
+	  (restore val) 			; val には Fib(n − 1) が入る
+	  (restore continue)
+	  (assign val ; Fib(n − 1) + Fib(n − 2)
+			  (op +) (reg val) (reg n))
+	  (goto (reg continue)) 										; 呼び出し元に戻る、答えはval の中
+	  immediate-answer
+	  (assign val (reg n)) 										; 基底の場合: Fib(n) = n
+	  (goto (reg continue))
+	  fib-done)))
+(set-register-contents! fib-machine 'n 10)
+(get-register-contents fib-machine 'n)
+(start fib-machine)
+(get-register-contents fib-machine 'n)
+;; gosh> 21
